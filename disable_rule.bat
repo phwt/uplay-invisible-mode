1cbc79ed2d6ff7cfbb8e00a3e228453b795c28ba
phwt <phwt.smwt@gmail.com>
2018-05-01 14:50:19 +0700

    Add admin right check

--- disable_rule.bat
+++ disable_rule.bat
@@ -1,4 +1,12 @@
 @echo off
-netsh advfirewall firewall set rule name="UplayOfflineMode" new enable=no
-echo "UplayOfflineMode" rule disabled
-pause
+
+net session >nul 2>&1
+if %errorLevel% == 0 (
+	netsh advfirewall firewall set rule name="UplayOfflineMode" new enable=no
+	echo "UplayOfflineMode" rule enabled
+	pause
+) else (
+	echo Administrator Priviliages required to run this task.
+	pause
+	exit
+)
