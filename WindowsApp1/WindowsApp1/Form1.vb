aee716ba4971a4af1ee7f12b173b3417ef912dd9
phwt <phwt.smwt@gmail.com>
2018-05-10 14:28:46 +0700

    commit

---
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub OpenFileDialog2_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Shell("cmd.exe /k" + "netsh advfirewall firewall add rule name='UplayOfflineMode' dir=out action=block program='%ProgramFiles% (x86)\Ubisoft\Ubisoft Game Launcher\upc.exe' enable=no")
+        ' Shell("cmd.exe /k" + "echo kwai all")
+    End Sub
+End Class
